13e9f002596d62518de83391366c5186b21aa3d4
Kseniia Dukelska <kseniaduk@gmail.com>
2023-02-18 16:14:15 +0100

    start working with scss

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -5,6 +5,7 @@ const sass = gulpSass(dartSass);
 
 export const scss = () => {
     return app.gulp.src(app.path.src.scss, { sourcemaps: true })
+    .pipe(app.plugins.replace(/@img\//g, '../img/'))
     .pipe(sass({
         outputStyle: 'expanded'
     }))
